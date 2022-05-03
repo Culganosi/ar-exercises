295e8145288818f99a4eb11a1871d36448ba62ce
Culganosi <culgan_@hotmail.com>
2022-05-02 23:57:21 -0400

    Exercise 2: Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+#Find by id - This can either be a specific id (1)
+#Person.find(1)          # returns the object for ID = 1
+@store1 = Store.find(1) 
+@store2 = Store.find(2)
+#update(id, attributes)
+@store1 = Store.update(1, name: "Montreal")
+
+puts @store1
